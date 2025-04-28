e3d1f9f54e01bf0b9a881fd76f39cd8021918713
Alex Eastabrook <alex.eastabrook@newburyinnovation.co.uk>
2025-04-28 10:52:17 +0100

    Purge files that hurt compiling.
    GPT says -
    Why?
    
    ed25519_psa.c depends on <psa/crypto.h>.
    and thats extra to do, so purge it
    ram_load.c  - Is if your loading from ram. Who would have thought.

--- bootloader/Core/Src/main.c
+++ bootloader/Core/Src/main.c
@@ -342,23 +342,6 @@ void assert_failed(uint8_t *file, uint32_t line)
 
 
 ///// Upstream Example 
-
-//! A very naive implementation of the newlib _sbrk dependency function
-caddr_t _sbrk(int incr);
-caddr_t _sbrk(int incr) {
-  static uint32_t s_index = 0;
-  static uint8_t s_newlib_heap[2048] __attribute__((aligned(8)));
-
-  if ((s_index + (uint32_t)incr) <= sizeof(s_newlib_heap)) {
-    EXAMPLE_LOG("Out of Memory!");
-    return 0;
-  }
-
-  caddr_t result = (caddr_t)&s_newlib_heap[s_index];
-  s_index += (uint32_t)incr;
-  return result;
-}
-
 __attribute__((noinline))
 static void prv_enable_vfp( void ){
   volatile uint32_t *cpacr = (volatile uint32_t *)0xE000ED88;
