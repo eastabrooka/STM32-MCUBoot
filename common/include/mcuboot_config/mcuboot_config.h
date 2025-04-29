c4ae40d3964e9994884f2b2cad515481deba2cbc
Alex Eastabrook <alex.eastabrook@newburyinnovation.co.uk>
2025-04-29 12:09:11 +0100

    RSA 2048

--- common/include/mcuboot_config/mcuboot_config.h
+++ common/include/mcuboot_config/mcuboot_config.h
@@ -5,20 +5,21 @@
 #define MCUBOOT_BOOTSTRAP                  1
 
 // === Signature method ===
-#define MCUBOOT_SIGN_EC256                  1
-#define MCUBOOT_USE_MBED_TLS                1
+#define MCUBOOT_SIGN_RSA                   1
+#define MCUBOOT_SIGN_RSA_LEN              2048
+#define MCUBOOT_USE_MBED_TLS              1
 
 // === Upgrade method ===
-#define MCUBOOT_SWAP_USING_MOVE             1
+#define MCUBOOT_SWAP_USING_MOVE           1
 
 // === Validation ===
-#define MCUBOOT_VALIDATE_PRIMARY_SLOT       1
-#define MCUBOOT_IMAGE_NUMBER                1
-#define MCUBOOT_MAX_IMG_SECTORS              256
+#define MCUBOOT_VALIDATE_PRIMARY_SLOT     1
+#define MCUBOOT_IMAGE_NUMBER              1
+#define MCUBOOT_MAX_IMG_SECTORS           256
 
 // === Logging and Assert ===
-#define MCUBOOT_HAVE_LOGGING                1
-#define MCUBOOT_HAVE_ASSERT_H               1
+#define MCUBOOT_HAVE_LOGGING              1
+#define MCUBOOT_HAVE_ASSERT_H             1
 
 // === Watchdog ===
 #define MCUBOOT_WATCHDOG_FEED()
